8b2610c03e9d9c918ccd2390fd46f1f419313a19
Yiyun Wei <ywei21@ndsj.org>
2022-04-22 08:02:47 -0400

    added enemies scattered throughout the map that all use pathfinding + can be destroyed by player

---
+++ code/enemy_class.py
@@ -0,0 +1,26 @@
+import copy
+#from main_game import *
+from a_star import *
+
+class Enemy:
+    def __init__(self, x, y):
+        self.enemyAlive = True
+        #cell is relative to entire game map
+        self.enemyCellX = x
+        self.enemyCellY = y
+        #position is relative to entire game map
+        self.enemyPosX = None
+        self.enemyPosY = None
+
+    def setCoords(self, x, y):
+        self.enemyPosX = x
+        self.enemyPosY = y
+    
+    def move(self, destX, destY, limit, x0, y0, x1, y1):
+        d = ((self.enemyPosX - destX)**2 + (self.enemyPosY - destY)**2)**0.5
+        if (d > limit):
+            self.enemyPosX -= (self.enemyPosX-(x0+x1)/2)*0.2
+            self.enemyPosY -= (self.enemyPosY-(y0+y1)/2)*0.2
+        else:
+            self.enemyPosX -= (self.enemyPosX-destX)*0.2
+            self.enemyPosY -= (self.enemyPosY-destY)*0.2
